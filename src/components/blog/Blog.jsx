6baffb1125c99040520d920c503e7701120e689e
shafayetullah7 <shafayet769@gmail.com>
2023-05-04 18:57:40 +0600

    done

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,42 +1,46 @@
 import React from 'react';
 
 const Blog = () => {
+    const ref = React.createRef();
     return (
         <div>
-            <h1 className='md:text-6xl text-4xl text-center font-bold mt-20'>Question & Answers</h1>
-            <div className='lg:px-24 px-2 mt-16'>
-                <div className='border border-[#A3BB98] rounded-lg p-5 my-3'>
-                    <h2 className='text-xl font-bold'>What are the differences between uncontrolled and controlled components</h2>
-                    <p className='text-gray-600 mt-5'>
-                        The main difference between uncontrolled and controlled components is how they handle data. In uncontrolled components data is handled directly by DOM. Whereas in controlled components data is handled by react components. With controlled components it is easier and simpler to handle user input compare to uncontrolled components.
-                    </p>
-                </div>
-                <div className='border border-[#A3BB98] rounded-lg p-5 my-3'>
-                    <h2 className='text-xl font-bold'>How to validate React props using PropTypes</h2>
-                    <div className='text-gray-600 mt-5'>
-                        <p>We can define datatypes for the props that a component recieves. To do that we access props like:</p>
-                        <p>
-                            MyComponent.propTypes = &#123;
-                            title: PropTypes.string.isRequired,
-                            description: PropTypes.string.isRequired
-                            &#125;
+            <div ref={ref}>
+                <h1 className='md:text-6xl text-4xl text-center font-bold mt-20'>Question & Answers</h1>
+                <div className='lg:px-24 px-2 mt-16'>
+                    <div className='border border-[#A3BB98] rounded-lg p-5 my-3'>
+                        <h2 className='text-xl font-bold'>What are the differences between uncontrolled and controlled components</h2>
+                        <p className='text-gray-600 mt-5'>
+                            The main difference between uncontrolled and controlled components is how they handle data. In uncontrolled components data is handled directly by DOM. Whereas in controlled components data is handled by react components. With controlled components it is easier and simpler to handle user input compare to uncontrolled components.
+                        </p>
+                    </div>
+                    <div className='border border-[#A3BB98] rounded-lg p-5 my-3'>
+                        <h2 className='text-xl font-bold'>How to validate React props using PropTypes</h2>
+                        <div className='text-gray-600 mt-5'>
+                            <p>We can define datatypes for the props that a component recieves. To do that we access props like:</p>
+                            <p>
+                                MyComponent.propTypes = &#123;
+                                title: PropTypes.string.isRequired,
+                                description: PropTypes.string.isRequired
+                                &#125;
+                            </p>
+                        </div>
+                    </div>
+                    <div className='border border-[#A3BB98] rounded-lg p-5 my-3'>
+                        <h2 className='text-xl font-bold'>What is the difference between nodejs and express js</h2>
+                        <p className='text-gray-600 mt-5'>
+                            Node.js is a server-side JavaScript runtime environment built on top of Chrome's V8 engine. Node.js provides a platform for running JavaScript code outside of a web browser. Express.js is a web application framework built on top of Node.js. Express.js simplifies the process of creating web applications using Node.js. It provides a set of features for building web applications, such as routing, middleware, and templating engines etc.
+                        </p>
+                    </div>
+                    <div className='border border-[#A3BB98] rounded-lg p-5 my-3'>
+                        <h2 className='text-xl font-bold'>What is a custom hook, and why will you create a custom hook?</h2>
+                        <p className='text-gray-600 mt-5'>
+                        In React, custom hook is a function that allows to reuse logic across different components. It is a way to share functionality between components without repeating the same code in each component. we can create custom hook to handle form input, fetch data from an API
                         </p>
                     </div>
                 </div>
-                <div className='border border-[#A3BB98] rounded-lg p-5 my-3'>
-                    <h2 className='text-xl font-bold'>What is the difference between nodejs and express js</h2>
-                    <p className='text-gray-600 mt-5'>
-                        Node.js is a server-side JavaScript runtime environment built on top of Chrome's V8 engine. Node.js provides a platform for running JavaScript code outside of a web browser. Express.js is a web application framework built on top of Node.js. Express.js simplifies the process of creating web applications using Node.js. It provides a set of features for building web applications, such as routing, middleware, and templating engines etc.
-                    </p>
-                </div>
-                <div className='border border-[#A3BB98] rounded-lg p-5 my-3'>
-                    <h2 className='text-xl font-bold'>What is a custom hook, and why will you create a custom hook?</h2>
-                    <p className='text-gray-600 mt-5'>
-                    In React, custom hook is a function that allows to reuse logic across different components. It is a way to share functionality between components without repeating the same code in each component. we can create custom hook to handle form input, fetch data from an API
-                    </p>
-                </div>
             </div>
         </div>
+        
     );
 };
 
--- src/components/chefAndRecipes/ChefRecipes.jsx
+++ src/components/chefAndRecipes/ChefRecipes.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import { useLoaderData } from 'react-router-dom';
-import LazyLoad from 'react-lazyload';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 import Recipe from './Recipe';
 import { AuthContext } from '../../authProvider/AuthProvider';
 
@@ -15,18 +15,22 @@ const ChefRecipes = () => {
     console.log(chef);
     console.log(recipes);
     return (
-        <div className='px-24 mt-24'>
+        <div className='lg:px-24 px-2 mt-24'>
             <div>
-                <div className='bg-[#A3BB98] bg-opacity-20 p-5 rounded-xl flex gap-10 lg:h-[500px]'>
+                <div className='bg-[#A3BB98] bg-opacity-20 p-5 rounded-xl flex flex-col md:flex-row gap-10 lg:h-[500px]'>
                     <div className='w-full'>
-                    <LazyLoad className='h-full'>
-                        <img className='w-full h-full object-cover object-top rounded-lg shadow-xl' src={chef.chef_picture} alt="" />
-                    </LazyLoad>
+                    <div className='w-full h-full'>
+                        <LazyLoadImage
+                        className='w-full h-[300px] md:h-full object-cover object-top'
+                        src={chef.chef_picture} // use normal <img> attributes as props
+                         />
+                        
+                    </div>
                         {/* <img className='w-full h-full object-cover object-top rounded-lg shadow-xl' src={chef.chef_picture} alt="" /> */}
                     </div>
                     <div className='w-full flex items-center'>
                         <div>
-                            <p className='text-7xl font-bold'>{chef.chef_name}</p>
+                            <p className='md:text-7xl text-4xl font-bold'>{chef.chef_name}</p>
                             <p className='md:w-[90%] text-sm text-gray-600 tracking-wider mt-10 text-justify indent-10'>{chef.description}</p>
                             <p className='mt-16 font-bold'>Experience: <span className='text-gray-500 font-normal'>{chef.years_of_experience} years</span></p>
                             <p className='mt-1 font-bold'>Number of recipes: <span className='text-gray-500 font-normal'>{chef.num_recipes}</span></p>
